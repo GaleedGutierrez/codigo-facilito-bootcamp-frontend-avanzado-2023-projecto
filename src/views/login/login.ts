bbb1bd9fa79b851fbcd6adc2fdfeb8595810252c
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-10-25 12:59:41 -0300

    :white_check_mark: Implementación del login con firebase listo

--- src/views/login/login.ts
+++ src/views/login/login.ts
@@ -6,11 +6,23 @@ import '@components/common/text-field/TextField';
 
 // eslint-disable-next-line import/no-duplicates
 import { TextField } from '@components/common/text-field/TextField';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+import { FIREBASE_AUTH } from '@/firebase';
 
 // Nodes
 const EMAIL = document.querySelector('#email') as TextField;
 const PASSWORD = document.querySelector('#password') as TextField;
 const FORM = document.querySelector('#form-login') as HTMLFormElement;
+const INPUTS_CONTAINER = Array.from(
+	document.querySelectorAll('text-field') as NodeListOf<TextField>
+);
+
+const wrongCredentials = () => {
+	INPUTS_CONTAINER.forEach((textField) => {
+		textField?.container?.classList.add('is-invalid');
+	});
+};
 
 FORM.addEventListener('submit', (event) => {
 	event.preventDefault();
@@ -20,30 +32,15 @@ FORM.addEventListener('submit', (event) => {
 
 	if (!USER_PASSWORD || !USER_EMAIL) return;
 
-	// signInWithEmailAndPassword(FIREBASE_AUTH, USER_EMAIL, USER_PASSWORD)
-	// 	.then((userCredential) => {
-	// 		console.log(userCredential.user);
-	// 	})
-	// 	.catch((error) => {
-	// 		console.error(error);
-	// 	});
+	signInWithEmailAndPassword(FIREBASE_AUTH, USER_EMAIL, USER_PASSWORD)
+		.then((userCredential) => {
+			INPUTS_CONTAINER.forEach((textField) => {
+				textField?.container?.classList.remove('is-invalid');
+			});
+			console.log(userCredential.user);
+		})
+		.catch((error) => {
+			wrongCredentials();
+			console.error(error);
+		});
 });
-
-// login
-// FORM.addEventListener('submit', (event) => {
-// 	event.preventDefault();
-
-// 	const USER_EMAIL = EMAIL.input?.value;
-// 	const USER_PASSWORD = PASSWORD.input?.value;
-
-// 	if (!USER_PASSWORD || !USER_EMAIL) return;
-
-// 	createUserWithEmailAndPassword(FIREBASE_AUTH, USER_EMAIL, USER_PASSWORD)
-// 		.then((userCredential) => {
-// 			console.log(userCredential.user);
-// 		})
-// 		.catch((error) => {
-// 			console.error(error);
-// 		});
-// 	console.log({ USER_EMAIL, USER_PASSWORD });
-// });
