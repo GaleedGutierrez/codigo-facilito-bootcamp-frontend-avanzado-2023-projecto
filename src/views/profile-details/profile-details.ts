9f164d1b17e9557d097c05db01956c1c2b4576de
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-11-02 19:48:43 -0300

    :lipstick: Pantalla de profile details lista

--- src/views/profile-details/profile-details.ts
+++ src/views/profile-details/profile-details.ts
@@ -5,3 +5,4 @@ import '@/components/common/tab-link/Tab';
 import '@/components/common/preview-link/previewLink';
 import '@/components/common/text-field/TextField';
 import '@/components/common/upload-img/UploadImg';
+import '@/components/common/publish-link/Publish';
